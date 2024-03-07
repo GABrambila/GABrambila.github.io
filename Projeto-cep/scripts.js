96bf1be318fcd9f39883622fedda33f8cc65e832
GABrambila <brambila.gabriel26@gmail.com>
2024-03-07 20:50:29 -0300

    addDarkMode

--- Projeto-cep/scripts.js
+++ Projeto-cep/scripts.js
@@ -13,17 +13,35 @@ $(document).ready(function(){
             alert('Informe um CEP válido!');
             return false; 
         }
+
+        loading();
+        setTimeout(function(){
+
         $.ajax({
             url:'https://viacep.com.br/ws/'+cep+'/json/',
             type: 'GET', 
             dataType: 'json', 
             success: function(data){
-                console.log(data); 
+                console.log(data);
+                $('body').find('#result').html('<h2>Resultado da consulta!</h2>'
+                                                + '<p>CEP: '+data.cep+'</p>'
+                                                + '<p>Logradouro: '+data.logradouro+'</p>'
+                                                +'<p>Bairro: '+data.bairro+'</p>'
+                                                +'<p>Cidade: '+data.localidade+'</p>'                                                
+                                                +'<p>Estado: '+data.uf+'</p>'                                                
+                                                +'<p>IBGE: '+data.ibge+'</p>'                                                
+                                                );  
 
             }
         })
+    },2500);        
     }
 
+    function loading(){
+        $('#result').html('<img src="Loading_blu.gif" />')
+    }
 
-
+    $('body').on('click', '#modo-noturno', function(){
+          $('body').toggleClass('dark-mode'); 
+    });
 })
