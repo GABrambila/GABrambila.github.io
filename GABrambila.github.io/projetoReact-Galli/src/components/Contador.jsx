ef8c206a188faa7562fb1a74c043f71cad5070b1
GABrambila <brambila.gabriel26@gmail.com>
2024-03-20 22:29:56 -0300

    projectReact-Galli-addCounter

---
+++ GABrambila.github.io/projetoReact-Galli/src/components/Contador.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+
+
+const Contador = () =>{
+    const [count, setCount] = useState(0); 
+    const [text, setText] = useState("");
+
+    const incrementar = ()=>{
+        setCount(count + 1);
+    };
+
+    const decrementar = ()=>{
+        setCount(count - 1);
+    };
+
+    const showText =(event)=>{
+        setText(event.target.value); 
+
+    }; 
+
+
+    return(
+        <main>
+            <p>
+               Digite algo: 
+                <input type="text" onChange={showText}/>
+                <p> O texto digitado é: {text} </p> 
+            </p>
+     
+               Contador: {count}               
+            <p>
+
+                <button onClick={incrementar}>Incrementar</button>
+                <button onClick={decrementar}>Decrementar</button>
+            </p>
+        </main>
+    )
+}
+export default Contador; 
