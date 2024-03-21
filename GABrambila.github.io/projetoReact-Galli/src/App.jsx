c20e10678bfe10d053b1dfe5d8e6105c798e574b
GABrambila <brambila.gabriel26@gmail.com>
2024-03-21 19:54:24 -0300

    add-darkMode

--- GABrambila.github.io/projetoReact-Galli/src/App.jsx
+++ GABrambila.github.io/projetoReact-Galli/src/App.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Contador from "./components/Contador";
 
 const App = () => {
+  const [darkmode, setDarkMode] = useState(false); 
+
+  const alteraDarkMode = ()=>{
+    console.log(1312); 
+    setDarkMode(!darkmode);
+};
+
   return (
-    <>
+    <div className={darkmode ? "modo-escuro" : ""}>
       <Header />
       <Contador/>
+      Alterar cor de fundo
+      <button onClick={alteraDarkMode}>Alterar</button>
       <Footer />
-    </>
+    </div>
   );
 };
 
