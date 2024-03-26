89f194df1dcea0bc157a3f8f40247169a56fc817
GABrambila <brambila.gabriel26@gmail.com>
2024-03-26 20:56:41 -0300

    aulaGalli-roteamentoProps

---
+++ GABrambila.github.io/roteamento-props/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+
+
+const App = () => {
+  return <>
+
+    <Header />
+    <Main>
+    </Main>
+    <Footer />
+
+  </>;
+};
+
+
+export default App; 
